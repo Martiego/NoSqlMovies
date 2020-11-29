4ef91a1982a1e642f95014978236b9bafff91795
Kacper <kacper.swiercz00@gmail.com>
2020-11-29 10:25:53 +0100

    Add getters and setters to SpokenLanguage and ProductionCompany

--- src/main/java/nosql/rest/mongo/nosql/model/Movie.java
+++ src/main/java/nosql/rest/mongo/nosql/model/Movie.java
@@ -14,27 +14,26 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
-@Document(collection = "MovieT")
+@Document(collection = "Movie")
 public class Movie {
     @Id
     private ObjectId _id;
     private long budget;
-    //TODO Kolekcje nie działają, IllegalArgumentException
+    // TODO id w kolekcjach jest 0
     private List<Genre> genres;
     private String homepage;
     private int id;
-//    private Keyword[] keywords;
+    private List<Keyword> keywords;
     private String original_language;
     private String original_title;
     private String overview;
     private double popularity;
-//    private ProductionCompany[] production_companies;
-//    private ProductionCountry[] production_countries;
-    // TODO trzeba przekonwertować datę ze stringa na date
+    private List<ProductionCompany> production_companies;
+    private List<ProductionCountry> production_countries;
     private Date release_date;
     private long revenue;
-    private int runtime;
-//    private SpokenLanguage[] spoken_languages;
+    private Integer runtime;
+    private List<SpokenLanguage> spoken_languages;
     private String status;
     private String tagline;
     private String title;
--- src/main/java/nosql/rest/mongo/nosql/model/fields/ProductionCompany.java
+++ src/main/java/nosql/rest/mongo/nosql/model/fields/ProductionCompany.java
@@ -1,5 +1,12 @@
 package nosql.rest.mongo.nosql.model.fields;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class ProductionCompany {
     private int id;
     private String name;
