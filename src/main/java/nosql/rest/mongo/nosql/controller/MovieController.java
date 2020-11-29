ec76fa7a8ab5f582fafb31ab4938bc491dbd8dff
Patryk Kolanek <marti3go@gmail.com>
2020-11-29 12:37:57 +0100

    Add GET for ProductionCompanies and MoviesBetterThan

--- src/main/java/nosql/rest/mongo/nosql/controller/MovieController.java
+++ src/main/java/nosql/rest/mongo/nosql/controller/MovieController.java
@@ -1,12 +1,12 @@
 package nosql.rest.mongo.nosql.controller;
 
 import nosql.rest.mongo.nosql.model.Movie;
+import nosql.rest.mongo.nosql.model.fields.ProductionCompany;
 import nosql.rest.mongo.nosql.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class MovieController {
@@ -31,8 +31,32 @@ public class MovieController {
         return movieRepository.findById(id);
     }
 
+    @GetMapping("/findMoviesBetterThan/{rate}")
+    public List<Movie> getMoviesBetterThen(@PathVariable double rate) {
+        List<Movie> movies = movieRepository.findAll();
+        List<Movie> moviesBetterThan = new ArrayList<>();
+        for (Movie m : movies) {
+            if (m.getVote_average() > rate) {
+                moviesBetterThan.add(m);
+            }
+        }
+
+        return moviesBetterThan;
+    }
+
+    @GetMapping("/getAllProductionCompanies")
+    public Set<ProductionCompany> getAllProductionCompanies() {
+        List<Movie> movies = movieRepository.findAll();
+        Set<ProductionCompany> productionCompanies = new HashSet<>();
+        for (Movie m : movies) {
+            productionCompanies.addAll(m.getProduction_companies());
+        }
+
+        return productionCompanies;
+    }
+
     @DeleteMapping("/deleteMovie/{id}")
-    public  String deleteBook(@PathVariable int id) {
+    public String deleteBook(@PathVariable int id) {
         List<Movie> movies = movieRepository.findAll();
         for (Movie movie : movies) {
             if (movie.getId() == id) {
--- src/main/java/nosql/rest/mongo/nosql/model/fields/ProductionCompany.java
+++ src/main/java/nosql/rest/mongo/nosql/model/fields/ProductionCompany.java
@@ -1,5 +1,6 @@
 package nosql.rest.mongo.nosql.model.fields;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class ProductionCompany {
     private int id;
     private String name;
